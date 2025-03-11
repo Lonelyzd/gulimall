83bf7f467cec4b89ac0567dc8235ccdcf47f44a0
IceBlue <1156607853@qq.com>
2025-03-11 22:16:21 +0800

    网关模块添加日志打印

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/logs/GatewayConfig.java
@@ -0,0 +1,19 @@
+package com.atguigu.gulimall.gateway.config.logs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.server.WebFilter;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE) //过滤器顺序
+    public WebFilter webFilter() {
+        return (exchange, chain) -> chain.filter(new PayloadServerWebExchangeDecorator(exchange));
+    }
+
+}
+
---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/logs/ResponseParamsHandle.java
@@ -0,0 +1,15 @@
+package com.atguigu.gulimall.gateway.config.logs;
+
+import org.slf4j.Logger;
+import org.springframework.core.io.buffer.DataBuffer;
+
+public class ResponseParamsHandle {
+
+    public static <T extends DataBuffer> T chain(Logger log, T buffer) {
+        ParamsUtils.BodyDecorator bodyDecorator = ParamsUtils.buildBodyDecorator(buffer);
+        // 参数校验 和 参数打印
+        log.info("Payload: {}", bodyDecorator.getBody());
+        return (T) bodyDecorator.getDataBuffer();
+    }
+
+}
